b31edee6236c4b01ef66adb5aeffc543b7e6443e
김종연 <bellopenerr@gmail.com>
2025-07-28 23:55:34 +0900

    feat: 배열 방식으로 라우터 관리 개선

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,49 @@
-import * as http from 'http';
+import * as http from 'http'
+
+export type HttpRequest = http.IncomingMessage
+export type HttpResponse = http.ServerResponse<HttpRequest>
+export type RouteMapping = {
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+  url: string
+  handler: (req: HttpRequest, res: HttpResponse) => void
+}
+
+const getHome = (req: HttpRequest, res: HttpResponse) => {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  res.end('This is home')
+}
+
+const getAbout = (req: HttpRequest, res: HttpResponse) => {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  res.end('This is about us')
+}
+
+const getContact = (req: HttpRequest, res: HttpResponse) => {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  res.end('This is our contact')
+}
+
+/* Http Method와 Url, Handler를 매핑한다. */
+const routes: RouteMapping[] = [
+  { method: 'GET', url: '/', handler: getHome },
+  { method: 'GET', url: '/about', handler: getAbout },
+  { method: 'GET', url: '/contact', handler: getContact },
+]
 
 const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  if (method === 'GET' && url === '/') {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Welcome to homepage');
+  const { url, method } = req
+
+  const route = routes.find((route) => route.url === url && route.method === method)
+
+  if (route) {
+    /* route가 존재한다면 handler를 호출한다 */
+    route.handler(req, res)
+  } else {
+    res.writeHead(404, { 'content-type': 'text/plain' })
+    res.end('Page not found')
   }
-});
+})
 
 server.listen(3000, () => {
-  console.log('🚀 server running at http://localhost:3000');
-});
+  console.log('🚀 server running at http://localhost:3000')
+})
