5c98978cbd16f9fcfa1024ba093e432e25a682d4
yeonic <112757968+yeonic@users.noreply.github.com>
2025-08-20 00:20:26 +0900

    feat: add createRouter

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as http from 'http'
 import * as url from 'url'
 
 import { assert } from 'console'
-import { Router } from './types'
+import { AnyHandler, ErrorHandler, Handler, Method, Router } from './types'
 
 export function createApplication() {
   const app = createRouter()
@@ -17,7 +17,7 @@ export function createApplication() {
   }
 
   app.listen = function () {
-    http.createServer((req, res) => {
+    const server = http.createServer((req, res) => {
       assert(req.url !== undefined)
 
       const { pathname, query } = url.parse(req.url!, true)
@@ -44,12 +44,116 @@ export function createApplication() {
         })
         res.end(JSON.stringify(data))
       }
+
+      // TODO: app.handle 호출
     })
+
+    return app.listen?.apply(server)
   }
 
   return app
 }
 
 function createRouter(): Router {
-  return {}
+  const router: Router = function (req, res, next) {
+    router.handle(req, res, next)
+  }
+
+  router.stack = []
+
+  router.use = function (...args: any[]) {
+    const handler: AnyHandler = args.length === 1 ? args[0] : args[1]
+    const path: string = args.length === 1 ? '/' : args[0]
+
+    assert(router.stack)
+    router.stack!.push({
+      method: 'use',
+      path,
+      handler,
+      isErrorHandler: handler.length === 4,
+    })
+  }
+
+  const methods = Object.values(Method)
+  methods.forEach((method) => {
+    router[method] = function (path: string, ...handlers: Handler[]) {
+      assert(router.stack)
+      handlers.forEach((handler) => {
+        router.stack!.push({
+          method,
+          path,
+          handler,
+          isErrorHandler: false,
+        })
+      })
+    }
+  })
+
+  router.handle = function (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+    out: (err: Error) => void
+  ): void {
+    let idx = 0
+    const { pathname } = req
+
+    function next(err?: Error): void {
+      if (idx >= router.stack!.length) return out && out(err!)
+
+      assert(router.stack)
+      const layer = router.stack![idx++]
+      const { method, path: layerPath, handler, isErrorHandler } = layer
+
+      if (err) {
+        if (!isErrorHandler) return next(err)
+        if (layerPath !== '/' && !pathname?.startsWith(layerPath))
+          return next(err)
+
+        return (handler as ErrorHandler)(err, req, res, next)
+      }
+
+      if (method === 'use') {
+        if (layerPath !== '/' && !pathname?.startsWith(layerPath)) return next()
+
+        const subPath = pathname?.slice(layerPath.length) || '/'
+        const originPath = req.pathname
+        const originUrl = req.url
+
+        req.pathname = subPath
+        req.url = subPath + url.parse(req.url!, true).search || ''
+
+        const router = handler as Router
+        if (router.handle && router.handle === 'function') {
+          router.handle(req, res, function (err?: Error) {
+            req.pathname = originPath
+            req.url = originUrl
+            if (err) return next(err!)
+            next()
+          })
+        } else {
+          ;(handler as Handler)(req, res, function (err) {
+            req.pathname = originPath
+            req.url = originUrl
+            if (err) return next(err!)
+            next()
+          })
+        }
+        return
+      }
+
+      assert(req.method)
+      const m = req.method!.toLowerCase()
+      if (
+        method === 'all' ||
+        method === m /** && matchPath(layerPaht, pathname, req) */
+      ) {
+        return (handler as Handler)(req, res, next)
+      }
+
+      next()
+    }
+    next()
+  } satisfies Handler
+
+  return router
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import * as http from 'http'
 import { ParsedUrlQuery } from 'querystring'
 
 declare module 'http' {
@@ -22,10 +23,69 @@ declare module 'http' {
 
 export type Settings = { [name: string]: any }
 
-export type Router = {
+export const Method = {
+  GET: 'get',
+  POST: 'post',
+  PATCH: 'patch',
+  PUT: 'put',
+  DELETE: 'delete',
+  ALL: 'all',
+  USE: 'use',
+} as const
+
+export type Method = (typeof Method)[keyof typeof Method]
+
+export type Handler = (
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+  next: (err?: Error) => void
+) => void
+
+export type ErrorHandler = (
+  err: Error,
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+  next: (err?: Error) => void
+) => void
+
+export type AnyHandler = Handler | ErrorHandler
+
+// Type guards
+export function isErrorHandler(handler: AnyHandler): handler is ErrorHandler {
+  return handler.length === 4
+}
+
+export function isRouter(handler: any): handler is Router {
+  return typeof handler === 'function' && typeof handler.handle === 'function'
+}
+
+export interface Router {
+  (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+    next: (err?: Error) => void
+  ): void
+
   settings?: Settings
 
   set?: (name: string, value: string) => void
 
   listen?: () => void
+
+  stack?: {
+    method: Method
+
+    path: string
+
+    handler: AnyHandler
+
+    isErrorHandler: boolean
+  }[]
+
+  use?: {
+    (path: string, handler: AnyHandler): void
+    (handler: AnyHandler): void
+  }
+
+  [key: string]: any
 }
